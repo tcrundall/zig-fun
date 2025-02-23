5ba210eb0df7da247b3f601f9bb757c7d2e566c7
Tim Crundall <tim.crundall@gmail.com>
2025-02-23 20:49:06 +0100

    Improve 0001 solution

--- projects/leetcode/0001-two-sum.zig
+++ projects/leetcode/0001-two-sum.zig
@@ -5,17 +5,16 @@ const Pair = struct {
     y: usize,
 };
 
-fn twoSum(target: i32, nums: [*]i32, len: usize, allocator: std.mem.Allocator) !?Pair {
+fn twoSum(target: i32, nums: []i32, allocator: std.mem.Allocator) !?Pair {
     var map = std.AutoHashMap(i32, usize).init(allocator);
     defer map.deinit();
 
-    var i: usize = 0;
-    while (i < len) : (i += 1) {
-        const partner_ix = map.get(nums[i]);
+    for (nums, 0..) |num, i| {
+        const partner_ix = map.get(num);
         if (partner_ix) |p| {
             return Pair{ .x = p, .y = i };
         }
-        try map.put(target - nums[i], i);
+        try map.put(target - num, i);
     }
     return null;
 }
@@ -23,16 +22,16 @@ fn twoSum(target: i32, nums: [*]i32, len: usize, allocator: std.mem.Allocator) !
 pub fn main() !void {
     var gap = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gap.deinit();
-
     const allocator = gap.allocator();
 
     const target = 22;
     var nums: [4]i32 = [_]i32{ 2, 11, 7, 15 };
-    const many_nums_ptr: [*]i32 = &nums;
 
-    const result = try twoSum(target, many_nums_ptr, nums.len, allocator);
+    const result = try twoSum(target, nums[0..1], allocator);
     if (result) |r| {
         std.debug.print("{}, {}", .{ r.x, r.y });
+    } else {
+        std.debug.print("No pair found", .{});
     }
 }
 
@@ -48,7 +47,6 @@ const TestCase = struct {
 test "Test" {
     const allocator = testing.allocator;
     var nums = [_]i32{ 2, 11, 7, 15, -14, -5 };
-    const nums_ptr: [*]i32 = &nums;
 
     const test_cases = [_]TestCase{
         TestCase{ .target = 22, .expected_result = Pair{ .x = 2, .y = 3 } },
@@ -59,7 +57,7 @@ test "Test" {
     };
 
     for (test_cases) |tc| {
-        const actual_result = try twoSum(tc.target, nums_ptr, nums.len, allocator);
+        const actual_result = try twoSum(tc.target, &nums, allocator);
         try expectEqual(tc.expected_result, actual_result);
     }
 }

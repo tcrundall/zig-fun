b746d1976fe4c2ffef5ee43169a89171bc176892
Tim Crundall <tim.crundall@gmail.com>
2025-04-20 01:54:28 +0200

    Complete Step2 TODOs 7-14, conditional compilation

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/MathFunctions/MathFunctions.cxx
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/MathFunctions/MathFunctions.cxx
@@ -1,15 +1,18 @@
 #include "MathFunctions.h"
 
-// TODO 11: include cmath
+#include <cmath>
 
-// TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
+#if USE_MYMATH
 #include "mysqrt.h"
+#endif
 
 namespace mathfunctions {
-double sqrt(double x)
-{
-  // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
+double sqrt(double x) {
   // Otherwise, use std::sqrt.
+#if USE_MYMATH
   return detail::mysqrt(x);
+#else
+  return std::sqrt(x);
+#endif // USE_MYMATH
 }
-}
+} // namespace mathfunctions

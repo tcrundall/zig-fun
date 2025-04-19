f123e2acc6ab610b1eb8c0f59e427261008fbf60
Tim Crundall <tim.crundall@gmail.com>
2025-04-20 01:00:29 +0200

    Generate header with zig build
    * adapt todos 8-12 into zig build

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step1/build.zig
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step1/build.zig
@@ -1,49 +1,39 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+    const major_version = 1;
+    const minor_version = 0;
+
+    const tool_run1 = b.addSystemCommand(&.{"sed"});
+    tool_run1.addArgs(&.{b.fmt("s/@Tutorial_VERSION_MAJOR@/{d}/", .{major_version})});
+    tool_run1.addFileArg(b.path("TutorialConfig.h.in"));
+    const output1 = tool_run1.captureStdOut();
+
+    const tool_run2 = b.addSystemCommand(&.{"sed"});
+    tool_run2.addArgs(&.{b.fmt("s/@Tutorial_VERSION_MINOR@/{d}/", .{minor_version})});
+    tool_run2.addFileArg(output1);
+    const output2 = tool_run2.captureStdOut();
+
+    b.getInstallStep().dependOn(&b.addInstallFileWithDir(output2, .prefix, "TutorialConfig.h").step);
+
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
-
-    // We will also create a module for our other entry point, 'main.zig'.
     const exe_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        // .root_source_file = b.path("tutorial.cxx"),
         .target = target,
         .optimize = optimize,
     });
-
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
     const exe = b.addExecutable(.{
         .name = "tutorial_z",
         .root_module = exe_mod,
     });
     exe.addCSourceFile(.{ .file = .{ .cwd_relative = "tutorial.cxx" }, .flags = &.{} });
-    // exe.linkLibC();
+    exe.addIncludePath(.{ .cwd_relative = "zig-out" });
     exe.linkLibCpp(); // only need cpp libraries, it seems
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
+    const install_artifact = b.addInstallArtifact(exe, .{
+        .dest_dir = .{ .override = .prefix },
+    });
+    b.getInstallStep().dependOn(&install_artifact.step);
     const run_cmd = b.addRunArtifact(exe);
 
     // By making the run step depend on the install step, it will be run from the
@@ -57,22 +47,6 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // const exe_unit_tests = b.addTest(.{
-    //     .root_module = exe_mod,
-    // });
-    //
-    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    //
-    // // Similar to creating the run step earlier, this exposes a `test` step to
-    // // the `zig build --help` menu, providing a way for the user to request
-    // // running the unit tests.
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_exe_unit_tests.step);
 }

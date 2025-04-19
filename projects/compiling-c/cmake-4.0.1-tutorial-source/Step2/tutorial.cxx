d8681c57c1cd601f7d14eed415e2f8f0e4c6fccc
Tim Crundall <tim.crundall@gmail.com>
2025-04-20 01:25:01 +0200

    Complete Step2 TODOs 1-6, adding library

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/tutorial.cxx
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/tutorial.cxx
@@ -3,11 +3,10 @@
 #include <iostream>
 #include <string>
 
-// TODO 5: Include MathFunctions.h
+#include "MathFunctions.h"
 #include "TutorialConfig.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   if (argc < 2) {
     // report version
     std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
@@ -19,10 +18,8 @@ int main(int argc, char* argv[])
   // convert input to double
   double const inputValue = std::stod(argv[1]);
 
-  // TODO 6: Replace sqrt with mathfunctions::sqrt
-
   // calculate square root
-  double const outputValue = sqrt(inputValue);
+  double const outputValue = mathfunctions::sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;

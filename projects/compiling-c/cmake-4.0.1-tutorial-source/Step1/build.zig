254183afeaf4803928a5b404ca96474a862f3e7d
Tim Crundall <tim.crundall@gmail.com>
2025-04-20 17:11:37 +0200

    Generate header file properly (messily)
    * zig build system has methods for replacing values in "config headers"
    * use this instead of manual sed commands
    * use examples (perhaps out of date) found [here](https://ziggit.dev/t/build-system-tricks/3531)

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step1/build.zig
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step1/build.zig
@@ -4,17 +4,17 @@ pub fn build(b: *std.Build) void {
     const major_version = 1;
     const minor_version = 0;
 
-    const tool_run1 = b.addSystemCommand(&.{"sed"});
-    tool_run1.addArgs(&.{b.fmt("s/@Tutorial_VERSION_MAJOR@/{d}/", .{major_version})});
-    tool_run1.addFileArg(b.path("TutorialConfig.h.in"));
-    const output1 = tool_run1.captureStdOut();
-
-    const tool_run2 = b.addSystemCommand(&.{"sed"});
-    tool_run2.addArgs(&.{b.fmt("s/@Tutorial_VERSION_MINOR@/{d}/", .{minor_version})});
-    tool_run2.addFileArg(output1);
-    const output2 = tool_run2.captureStdOut();
-
-    b.getInstallStep().dependOn(&b.addInstallFileWithDir(output2, .prefix, "TutorialConfig.h").step);
+    // const tool_run1 = b.addSystemCommand(&.{"sed"});
+    // tool_run1.addArgs(&.{b.fmt("s/@Tutorial_VERSION_MAJOR@/{d}/", .{major_version})});
+    // tool_run1.addFileArg(b.path("TutorialConfig.h.in"));
+    // const output1 = tool_run1.captureStdOut();
+    //
+    // const tool_run2 = b.addSystemCommand(&.{"sed"});
+    // tool_run2.addArgs(&.{b.fmt("s/@Tutorial_VERSION_MINOR@/{d}/", .{minor_version})});
+    // tool_run2.addFileArg(output1);
+    // const output2 = tool_run2.captureStdOut();
+    //
+    // b.getInstallStep().dependOn(&b.addInstallFileWithDir(output2, .prefix, "TutorialConfig.h").step);
 
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -26,13 +26,24 @@ pub fn build(b: *std.Build) void {
         .name = "tutorial_z",
         .root_module = exe_mod,
     });
+
+    const cmake_cfg_header_cmd = b.addConfigHeader(.{ .style = .{ .cmake = b.path("TutorialConfig.h.in") } }, .{ .Tutorial_VERSION_MAJOR = major_version, .Tutorial_VERSION_MINOR = minor_version });
+    // const cmake_cfg_header_out = b.addInstallFile(b.path(cmake_cfg_header_cmd.output_file.path.?), "TutorialConfig.h");
+    const cmake_cfg_header_out = b.addInstallFile(cmake_cfg_header_cmd.getOutput(), "TutorialConfig.h");
+    // const generated_c_file = generate_c_file_cmd.addOutputFileArg("TutorialConfig.h");
+    // exe.addCSourceFile(.{ .file = b.path(cmake_cfg_header_out.dest_rel_path), .flags = &.{} });
+
     exe.addCSourceFile(.{ .file = .{ .cwd_relative = "tutorial.cxx" }, .flags = &.{} });
+    // exe.addCSourceFile(.{ .file = cmake_cfg_header_out.source, .flags = &.{} });
     exe.addIncludePath(.{ .cwd_relative = "zig-out" });
     exe.linkLibCpp(); // only need cpp libraries, it seems
+    exe.step.dependOn(&cmake_cfg_header_out.step);
 
     const install_artifact = b.addInstallArtifact(exe, .{
         .dest_dir = .{ .override = .prefix },
     });
+    install_artifact.step.dependOn(&cmake_cfg_header_out.step);
+    b.getInstallStep().dependOn(&cmake_cfg_header_out.step);
     b.getInstallStep().dependOn(&install_artifact.step);
     const run_cmd = b.addRunArtifact(exe);
 

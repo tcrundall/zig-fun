f09cd9d46a1c9ca88fc6a3d297beccb63b0668c4
Tim Crundall <tim.crundall@gmail.com>
2025-05-01 00:41:56 +0200

    Copy over current build.zig into Step5

---
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step5/build.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+const MAJOR_VERSION = 1;
+const MINOR_VERSION = 0;
+
+pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const windows = b.option(bool, "windows", "Target Microsoft Windows") orelse false;
+    const use_my_math = b.option(
+        bool,
+        "useMyMath",
+        "Use self implementation of sqrt",
+    ) orelse false;
+
+    const math_lib_name = "math_functions";
+
+    const lib_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe_mod = b.createModule(.{
+        .target = b.resolveTargetQuery(.{
+            .os_tag = if (windows) .windows else null,
+        }),
+        .optimize = optimize,
+    });
+    exe_mod.addImport(math_lib_name, lib_mod);
+
+    try buildLibrary(b, lib_mod, math_lib_name, use_my_math);
+    const exe = buildExecutable(b, exe_mod);
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
+
+fn buildLibrary(
+    b: *std.Build,
+    lib_mod: *std.Build.Module,
+    lib_name: []const u8,
+    use_my_math: bool,
+) !void {
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = lib_name,
+        .root_module = lib_mod,
+    });
+
+    var flags = std.ArrayList([]const u8).init(b.allocator);
+    defer flags.deinit();
+
+    if (use_my_math) {
+        try flags.append("-DUSE_MYMATH=1");
+        lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/mysqrt.cxx" } });
+    }
+
+    lib.addCSourceFile(.{
+        .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
+        .flags = flags.items,
+    });
+    lib.linkLibCpp();
+
+    b.installArtifact(lib);
+}
+
+fn buildExecutable(b: *std.Build, exe_mod: *std.Build.Module) *std.Build.Step.Compile {
+    const exe = b.addExecutable(.{
+        .name = "tutorial_z",
+        .root_module = exe_mod,
+    });
+
+    const cmake_cfg_header_cmd = b.addConfigHeader(
+        .{ .style = .{ .cmake = b.path("TutorialConfig.h.in") } },
+        .{
+            .Tutorial_VERSION_MAJOR = MAJOR_VERSION,
+            .Tutorial_VERSION_MINOR = MINOR_VERSION,
+        },
+    );
+
+    exe.addConfigHeader(cmake_cfg_header_cmd);
+    exe.addCSourceFile(.{ .file = .{ .cwd_relative = "tutorial.cxx" }, .flags = &.{} });
+    exe.addIncludePath(.{ .cwd_relative = "MathFunctions" });
+    exe.linkLibCpp();
+    return exe;
+}

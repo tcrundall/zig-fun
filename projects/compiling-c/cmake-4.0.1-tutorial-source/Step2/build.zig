a2062e9b39ede7bb98ff024045c5e647486a62a2
Tim Crundall <tim.crundall@gmail.com>
2025-04-30 23:04:55 +0200

    Tidy Step2 further - arraylist for flags

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/build.zig
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/build.zig
@@ -1,11 +1,17 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const major_version = 1;
-    const minor_version = 0;
+const MAJOR_VERSION = 1;
+const MINOR_VERSION = 0;
 
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const use_my_math = b.option(
+        bool,
+        "useMyMath",
+        "Use self implementation of sqrt",
+    ) orelse false;
+
     const lib_name = "math_functions";
 
     const lib_mod = b.createModule(.{
@@ -18,8 +24,8 @@ pub fn build(b: *std.Build) void {
     });
     exe_mod.addImport(lib_name, lib_mod);
 
-    buildLibrary(b, lib_name, lib_mod);
-    const exe = buildExecutable(b, exe_mod, major_version, minor_version);
+    try buildLibrary(b, lib_name, lib_mod, use_my_math);
+    const exe = buildExecutable(b, exe_mod);
 
     const run_cmd = b.addRunArtifact(exe);
     if (b.args) |args| {
@@ -31,29 +37,28 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 }
 
-fn buildLibrary(b: *std.Build, lib_name: []const u8, lib_mod: *std.Build.Module) void {
+fn buildLibrary(
+    b: *std.Build,
+    lib_name: []const u8,
+    lib_mod: *std.Build.Module,
+    use_my_math: bool,
+) !void {
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = lib_name,
         .root_module = lib_mod,
     });
 
-    const use_my_math = b.option(
-        bool,
-        "USE_MYMATH",
-        "Use self implementation of sqrt",
-    ) orelse false;
+    var flags = std.ArrayList([]const u8).init(b.allocator);
+    defer flags.deinit();
     if (use_my_math) {
-        lib.addCSourceFile(.{
-            .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
-            .flags = &.{"-DUSE_MYMATH=1"},
-        });
-    } else {
-        lib.addCSourceFile(.{
-            .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
-            .flags = &.{"-DUSE_MYMATH=0"},
-        });
+        try flags.append("-DUSE_MYMATH=1");
     }
+
+    lib.addCSourceFile(.{
+        .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
+        .flags = flags.items,
+    });
     lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/mysqrt.cxx" } });
     lib.linkLibCpp();
 
@@ -63,8 +68,6 @@ fn buildLibrary(b: *std.Build, lib_name: []const u8, lib_mod: *std.Build.Module)
 fn buildExecutable(
     b: *std.Build,
     exe_mod: *std.Build.Module,
-    major_version: u16,
-    minor_version: u16,
 ) *std.Build.Step.Compile {
     const exe = b.addExecutable(.{
         .name = "tutorial_z",
@@ -73,8 +76,8 @@ fn buildExecutable(
     const cmake_cfg_header_cmd = b.addConfigHeader(
         .{ .style = .{ .cmake = b.path("TutorialConfig.h.in") } },
         .{
-            .Tutorial_VERSION_MAJOR = major_version,
-            .Tutorial_VERSION_MINOR = minor_version,
+            .Tutorial_VERSION_MAJOR = MAJOR_VERSION,
+            .Tutorial_VERSION_MINOR = MINOR_VERSION,
         },
     );
     exe.addConfigHeader(cmake_cfg_header_cmd);

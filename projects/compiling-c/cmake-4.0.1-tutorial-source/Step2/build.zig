326653ff18d170098d0e6d53dcb253261738ae49
Tim Crundall <tim.crundall@gmail.com>
2025-04-30 22:43:15 +0200

    Tidy Step2 build.zig in similar fashion to Step4

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/build.zig
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step2/build.zig
@@ -6,25 +6,70 @@ pub fn build(b: *std.Build) void {
 
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const lib_name = "math_functions";
 
     const lib_mod = b.createModule(.{
         .target = target,
         .optimize = optimize,
     });
-
     const exe_mod = b.createModule(.{
         .target = target,
         .optimize = optimize,
     });
-    exe_mod.addImport("math_functions", lib_mod);
+    exe_mod.addImport(lib_name, lib_mod);
 
+    buildLibrary(b, lib_name, lib_mod);
+    const exe = buildExecutable(b, exe_mod, major_version, minor_version);
+
+    const run_cmd = b.addRunArtifact(exe);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
+
+fn buildLibrary(b: *std.Build, lib_name: []const u8, lib_mod: *std.Build.Module) void {
     const lib = b.addLibrary(.{
         .linkage = .static,
-        .name = "math_functions",
+        .name = lib_name,
         .root_module = lib_mod,
     });
 
-    // substitute cmake values
+    const use_my_math = b.option(
+        bool,
+        "USE_MYMATH",
+        "Use self implementation of sqrt",
+    ) orelse false;
+    if (use_my_math) {
+        lib.addCSourceFile(.{
+            .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
+            .flags = &.{"-DUSE_MYMATH=1"},
+        });
+    } else {
+        lib.addCSourceFile(.{
+            .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
+            .flags = &.{"-DUSE_MYMATH=0"},
+        });
+    }
+    lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/mysqrt.cxx" } });
+    lib.linkLibCpp();
+
+    b.installArtifact(lib);
+}
+
+fn buildExecutable(
+    b: *std.Build,
+    exe_mod: *std.Build.Module,
+    major_version: u16,
+    minor_version: u16,
+) *std.Build.Step.Compile {
+    const exe = b.addExecutable(.{
+        .name = "tutorial_z",
+        .root_module = exe_mod,
+    });
     const cmake_cfg_header_cmd = b.addConfigHeader(
         .{ .style = .{ .cmake = b.path("TutorialConfig.h.in") } },
         .{
@@ -32,44 +77,12 @@ pub fn build(b: *std.Build) void {
             .Tutorial_VERSION_MINOR = minor_version,
         },
     );
-
-    // store result in header file
-    const cmake_cfg_header_out = b.addInstallFile(cmake_cfg_header_cmd.getOutput(), "TutorialConfig.h");
-
-    const use_my_math = b.option(bool, "USE_MYMATH", "Use self implementation of sqrt") orelse false;
-    if (use_my_math) {
-        lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" }, .flags = &.{"-DUSE_MYMATH=1"} });
-    } else {
-        lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" }, .flags = &.{"-DUSE_MYMATH=0"} });
-    }
-    lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/mysqrt.cxx" } });
-    lib.linkLibCpp();
-
-    b.installArtifact(lib);
-
-    const exe = b.addExecutable(.{
-        .name = "tutorial_z",
-        .root_module = exe_mod,
-    });
-    exe.step.dependOn(&cmake_cfg_header_out.step);
-
+    exe.addConfigHeader(cmake_cfg_header_cmd);
     exe.addCSourceFile(.{ .file = .{ .cwd_relative = "tutorial.cxx" }, .flags = &.{} });
-    exe.addIncludePath(.{ .cwd_relative = "zig-out" }); // TODO: work out how to point this to "prefix"
-    // exe.addIncludePath(b.path(b.install_prefix)); // this _almost_ works. It works if prefix is custom set
     exe.addIncludePath(.{ .cwd_relative = "MathFunctions" });
     exe.linkLibCpp();
 
-    const install_artifact = b.addInstallArtifact(exe, .{
-        .dest_dir = .{ .override = .prefix },
-    });
-    b.getInstallStep().dependOn(&install_artifact.step);
-    const run_cmd = b.addRunArtifact(exe);
+    b.installArtifact(exe);
 
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    return exe;
 }

4721e6871fcc28d0ae5f7e0253294c04010c5290
Tim Crundall <tim.crundall@gmail.com>
2025-04-30 23:22:35 +0200

    Tidy Step4 further

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step4/build.zig
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step4/build.zig
@@ -1,25 +1,34 @@
 const std = @import("std");
+const MAJOR_VERSION = 1;
+const MINOR_VERSION = 0;
 
-pub fn build(b: *std.Build) void {
-    const major_version = 1;
-    const minor_version = 0;
-
+pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
     const windows = b.option(bool, "windows", "Target Microsoft Windows") orelse false;
+    const use_my_math = b.option(
+        bool,
+        "useMyMath",
+        "Use self implementation of sqrt",
+    ) orelse false;
 
     const math_lib_name = "math_functions";
 
-    const lib_mod = buildLibrary(b, optimize, target, math_lib_name);
-    const exe = buildExecutable(
-        b,
-        optimize,
-        windows,
-        math_lib_name,
-        lib_mod,
-        major_version,
-        minor_version,
-    );
+    const lib_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe_mod = b.createModule(.{
+        .target = b.resolveTargetQuery(.{
+            .os_tag = if (windows) .windows else null,
+        }),
+        .optimize = optimize,
+    });
+    exe_mod.addImport(math_lib_name, lib_mod);
+
+    try buildLibrary(b, lib_mod, math_lib_name, use_my_math);
+    const exe = buildExecutable(b, exe_mod);
 
     b.installArtifact(exe);
 
@@ -35,65 +44,44 @@ pub fn build(b: *std.Build) void {
 
 fn buildLibrary(
     b: *std.Build,
-    optimize: std.builtin.OptimizeMode,
-    target: std.Build.ResolvedTarget,
+    lib_mod: *std.Build.Module,
     lib_name: []const u8,
-) *std.Build.Module {
-    const lib_mod = b.createModule(.{
-        .target = target,
-        .optimize = optimize,
-    });
-
+    use_my_math: bool,
+) !void {
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = lib_name,
         .root_module = lib_mod,
     });
 
-    const use_my_math = b.option(bool, "USE_MYMATH", "Use self implementation of sqrt") orelse true;
-    var use_my_math_flag: []const u8 = "";
+    var flags = std.ArrayList([]const u8).init(b.allocator);
+    defer flags.deinit();
+
     if (use_my_math) {
-        use_my_math_flag = "-DUSE_MYMATH=1";
+        try flags.append("-DUSE_MYMATH=1");
+        lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/mysqrt.cxx" } });
     }
+
     lib.addCSourceFile(.{
         .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
-        .flags = &.{use_my_math_flag},
+        .flags = flags.items,
     });
-    lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/mysqrt.cxx" } });
     lib.linkLibCpp();
 
     b.installArtifact(lib);
-    return lib_mod;
 }
 
-fn buildExecutable(
-    b: *std.Build,
-    optimize: std.builtin.OptimizeMode,
-    windows: bool,
-    lib_name: []const u8,
-    lib_mod: *std.Build.Module,
-    major_version: u16,
-    minor_version: u16,
-) *std.Build.Step.Compile {
-    const exe_mod = b.createModule(.{
-        .target = b.resolveTargetQuery(.{
-            .os_tag = if (windows) .windows else null,
-        }),
-        .optimize = optimize,
-    });
-    exe_mod.addImport(lib_name, lib_mod);
-
+fn buildExecutable(b: *std.Build, exe_mod: *std.Build.Module) *std.Build.Step.Compile {
     const exe = b.addExecutable(.{
         .name = "tutorial_z",
         .root_module = exe_mod,
     });
 
-    // substitute cmake values
     const cmake_cfg_header_cmd = b.addConfigHeader(
         .{ .style = .{ .cmake = b.path("TutorialConfig.h.in") } },
         .{
-            .Tutorial_VERSION_MAJOR = major_version,
-            .Tutorial_VERSION_MINOR = minor_version,
+            .Tutorial_VERSION_MAJOR = MAJOR_VERSION,
+            .Tutorial_VERSION_MINOR = MINOR_VERSION,
         },
     );
 

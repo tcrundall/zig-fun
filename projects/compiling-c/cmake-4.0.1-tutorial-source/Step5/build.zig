e84ed68536e56ec4d93ef8860f84f4af7ad1e5bf
Tim Crundall <tim.crundall@gmail.com>
2025-05-01 15:53:08 +0200

    Add simple zig test example

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step5/build.zig
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step5/build.zig
@@ -2,6 +2,22 @@ const std = @import("std");
 const MAJOR_VERSION = 1;
 const MINOR_VERSION = 0;
 
+const test_targets = [_]std.Target.Query{
+    .{}, // native
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .linux,
+    },
+    // .{
+    //     .cpu_arch = .x86_64,
+    //     .os_tag = .windows,
+    // },
+    .{
+        .cpu_arch = .aarch64,
+        .os_tag = .macos,
+    },
+};
+
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
@@ -40,6 +56,19 @@ pub fn build(b: *std.Build) !void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const test_step = b.step("test", "Run unit tests");
+
+    for (test_targets) |test_target| {
+        const unit_tests = b.addTest(.{
+            .root_source_file = b.path("test.zig"),
+            .target = b.resolveTargetQuery(test_target),
+        });
+
+        const run_unit_tests = b.addRunArtifact(unit_tests);
+        run_unit_tests.skip_foreign_checks = true;
+        test_step.dependOn(&run_unit_tests.step);
+    }
 }
 
 fn buildLibrary(

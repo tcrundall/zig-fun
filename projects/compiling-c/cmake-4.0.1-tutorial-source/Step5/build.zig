60b5daea831feabbbb4e5994ca9ec5215f6a52fb
Tim Crundall <tim.crundall@gmail.com>
2025-05-01 17:03:45 +0200

    Tidy up Step5 build.zig - extreme clean code

--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step5/build.zig
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step5/build.zig
@@ -1,10 +1,32 @@
 const std = @import("std");
+
 const MAJOR_VERSION = 1;
 const MINOR_VERSION = 0;
 
+const MATH_LIB_NAME = "math_functions";
+
 pub fn build(b: *std.Build) !void {
-    const optimize = b.standardOptimizeOption(.{});
-    const target = b.standardTargetOptions(.{});
+    // Set up standard module options
+    const standard_opts = std.Build.Module.CreateOptions{
+        .optimize = b.standardOptimizeOption(.{}),
+        .target = b.standardTargetOptions(.{}),
+    };
+
+    // Compile and install modules
+    const lib = try compileAndInstallLibrary(b, standard_opts);
+    const exe = compileAndInstallExecutable(b, standard_opts, lib);
+    const tests = compileAndInstallTests(b, standard_opts);
+
+    // Configure steps
+    setupRunStep(b, exe);
+    setupTestStep(b, tests);
+}
+
+fn compileAndInstallLibrary(
+    b: *std.Build,
+    standard_opts: std.Build.Module.CreateOptions,
+) !*std.Build.Step.Compile {
+    // Set up library specific options
     const use_my_math = b.option(
         bool,
         "useMyMath",
@@ -16,91 +38,49 @@ pub fn build(b: *std.Build) !void {
         "Link library dynamically",
     ) orelse false;
 
-    const math_lib_name = "math_functions";
-
-    const lib_mod = b.createModule(.{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const exe_mod = b.createModule(.{
-        .target = target,
-        .optimize = optimize,
-    });
-    exe_mod.addImport(math_lib_name, lib_mod);
-
-    try buildLibrary(b, lib_mod, math_lib_name, use_my_math, dynamic_lib);
-    const exe = buildExecutable(b, exe_mod);
-
-    const run_cmd = b.addRunArtifact(exe);
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const test_step = b.step("test", "Run unit tests");
-    const test_mod = b.createModule(.{
-        .target = target,
-        .optimize = optimize,
-    });
-    const unit_test = b.addExecutable(.{ .name = "tests", .root_module = test_mod });
-    unit_test.linkLibC();
-    unit_test.linkLibCpp();
-    unit_test.addCSourceFile(.{
-        .file = b.path("tests/main.cpp"),
-        .flags = &.{"-pthread"},
-    });
-    const googletest_dep = b.dependency("googletest", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    unit_test.linkLibrary(googletest_dep.artifact("gtest"));
-    unit_test.linkLibrary(googletest_dep.artifact("gtest_main"));
-
-    b.installArtifact(unit_test);
-
-    const run_unit_test = b.addRunArtifact(unit_test);
-    test_step.dependOn(&run_unit_test.step);
-}
-
-fn buildLibrary(
-    b: *std.Build,
-    lib_mod: *std.Build.Module,
-    lib_name: []const u8,
-    use_my_math: bool,
-    dynamic_lib: bool,
-) !void {
+    // Initialize library
+    const lib_mod = b.createModule(standard_opts);
     const lib = b.addLibrary(.{
         .linkage = if (dynamic_lib) .dynamic else .static,
-        .name = lib_name,
+        .name = MATH_LIB_NAME,
         .root_module = lib_mod,
     });
 
+    // Handle conditional compilation
     var flags = std.ArrayList([]const u8).init(b.allocator);
     defer flags.deinit();
-
     if (use_my_math) {
         try flags.append("-DUSE_MYMATH=1");
         lib.addCSourceFile(.{ .file = .{ .cwd_relative = "MathFunctions/mysqrt.cxx" } });
     }
 
+    // Link soure files and dependencies
     lib.addCSourceFile(.{
         .file = .{ .cwd_relative = "MathFunctions/MathFunctions.cxx" },
         .flags = flags.items,
     });
     lib.linkLibCpp();
+
+    // Identify as install target
     b.installArtifact(lib);
+
+    return lib;
 }
 
-fn buildExecutable(b: *std.Build, exe_mod: *std.Build.Module) *std.Build.Step.Compile {
+fn compileAndInstallExecutable(
+    b: *std.Build,
+    standard_opts: std.Build.Module.CreateOptions,
+    lib: *std.Build.Step.Compile,
+) *std.Build.Step.Compile {
+    // Initialize executable, importing previously compiled library
+    const exe_mod = b.createModule(standard_opts);
+    exe_mod.addImport(MATH_LIB_NAME, lib.root_module);
     const exe = b.addExecutable(.{
         .name = "tutorial_z",
         .root_module = exe_mod,
     });
 
+    // Add preformatted header
     const cmake_cfg_header_cmd = b.addConfigHeader(
         .{ .style = .{ .cmake = b.path("TutorialConfig.h.in") } },
         .{
@@ -108,12 +88,53 @@ fn buildExecutable(b: *std.Build, exe_mod: *std.Build.Module) *std.Build.Step.Co
             .Tutorial_VERSION_MINOR = MINOR_VERSION,
         },
     );
-
     exe.addConfigHeader(cmake_cfg_header_cmd);
+
+    // Add source files, include headers and link dependencies
     exe.addCSourceFile(.{ .file = .{ .cwd_relative = "tutorial.cxx" }, .flags = &.{} });
     exe.addIncludePath(.{ .cwd_relative = "MathFunctions" });
     exe.linkLibCpp();
+
+    // Identify as install target
     b.installArtifact(exe);
 
     return exe;
+}
+
+fn compileAndInstallTests(b: *std.Build, standard_opts: std.Build.Module.CreateOptions) *std.Build.Step.Compile {
+    // Initialize tests
+    const test_mod = b.createModule(standard_opts);
+    const unit_test = b.addExecutable(.{ .name = "tests", .root_module = test_mod });
+
+    // Add source files, link dependencies
+    unit_test.addCSourceFile(.{
+        .file = b.path("tests/main.cpp"),
+    });
+    const googletest_dep = b.dependency("googletest", .{ // Does not accept nullable fields, so cannot directly pass standard_opts
+        .target = standard_opts.target.?,
+        .optimize = standard_opts.optimize.?,
+    });
+    unit_test.linkLibrary(googletest_dep.artifact("gtest"));
+    unit_test.linkLibrary(googletest_dep.artifact("gtest_main"));
+
+    // Identify as install target
+    // b.installArtifact(unit_test);
+
+    return unit_test;
+}
+
+fn setupRunStep(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    run_cmd.step.dependOn(b.getInstallStep());
+    run_step.dependOn(&run_cmd.step);
+}
+
+fn setupTestStep(b: *std.Build, tests: *std.Build.Step.Compile) void {
+    const test_step = b.step("test", "Run unit tests");
+    const run_tests = b.addRunArtifact(tests);
+    test_step.dependOn(&run_tests.step);
 }
--- projects/compiling-c/cmake-4.0.1-tutorial-source/Step5/tests/main.cpp
+++ projects/compiling-c/cmake-4.0.1-tutorial-source/Step5/tests/main.cpp
@@ -1,6 +1,5 @@
 #include "gtest/gtest.h"
 
-// #include <sdtio.h>
 TEST(SimpleTest, BasicAssertion) {
   EXPECT_EQ(1 + 1, 2);
   EXPECT_EQ(1 + 2, 3);
